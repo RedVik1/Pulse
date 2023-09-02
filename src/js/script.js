0191d53f5ac4329f4e1d476d1d567d4a11da1ebf
YehorMac <tyavinblaiz@gmail.com>
2023-09-02 11:53:43 +0300

    slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,7 @@
-var name = "Ivan";
-
-let number = 7;
-const pi = 3.14;
-
-number = 4;
-
-let leftBorderWidth;
-
-// number 
-// string 
-// true/false
-// null 
-// undefined 
-// let obj = {
-//     name: 'apple',
-//     color: 'green',
-//     weight: 200
-// }
-// Symbol
-
-// alert(1234)
-// console.log(number);
-// let answer = confirm("Вам есть 18?");
-// console.log(answer);
-
-// let answer = prompt("Вам есть 18?", "");
-// console.log(answer);
-
-// console.log(4 + 'vsvs');
-
-// let isChecked = false,
-//     isClose = false;
-
-// console.log(isChecked && isClose);
-
-// console.log(isChecked || isClose);
-
-// if (2*6 == 8*1) {
-//     console.log('Верно');
-// } else {
-//     console.log('Ошибка');
-// }
-
-// let answer = confirm("Вам есть 18?");
-// if (answer) {
-//     console.log('Проходите')
-// } else {
-//     console.log('Уходи')
-// }
-
-// const num = 50;
-
-// if (num < 49) {
-//     console.log('Неправильно')
-// } else if (num > 100) {
-//     console.log('Много')
-// } else {
-//     console.log('Верно')
-// }
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(i);
-// }
-
-function logging(a, b) {
-    console.log( a + b )
-}
-
-logging(3, 5);
-
-logging(6, 8);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight:true, 
+        prevArrow: '<button type="button" class="slick-prev"><img src=""></button>'
+    });
+});
