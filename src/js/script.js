1ef6808ca9dfb16a73b80269746e8b2c781f363b
YehorMac <tyavinblaiz@gmail.com>
2023-09-02 12:15:43 +0300

    carousel done

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight:true, 
-        prevArrow: '<button type="button" class="slick-prev"><img src=""></button>'
+        // adaptiveHeight: true, 
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowleft.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowright.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
