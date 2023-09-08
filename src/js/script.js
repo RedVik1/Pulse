129e94e2c45e53591043c7132e56628d3c71f2a3
YehorMac <tyavinblaiz@gmail.com>
2023-09-08 22:16:55 +0300

    php

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,26 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+380 (99) 999-99-99")
+
+    $('form').submit(function() {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultaton', '#order').fadeOut(slow);
+            $('.overlay', '#thanks').fadeIn(slow);
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
